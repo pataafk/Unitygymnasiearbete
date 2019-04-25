65c500af920455d59e0730cf7b7747e75ea01bfa
pataafk <42996902+pataafk@users.noreply.github.com>
2019-04-25 18:02:10 +0200

    da

---
+++ RPG 2D/Assets/scripts/Finishing_lvl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finishing_lvl : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("holla");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
